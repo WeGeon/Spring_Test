b4c37c08b6140d062118e43a041975739a3b3d2a
WeGeon <wigon99@gmail.com>
2021-08-23 16:58:23 +0900

    Web & Dto
    데이터베이스 접근

--- src/main/java/com/gun/websurvice/web/WebRestController.java
+++ src/main/java/com/gun/websurvice/web/WebRestController.java
@@ -1,14 +1,26 @@
 package com.gun.websurvice.web;
 
+import com.gun.websurvice.domain.posts.PostsRepository;
+import com.gun.websurvice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
 }
